4e83ae942f603372571292d2f447cf2031d3cfa6
eyssette <cedric.eyssette@gmail.com>
2023-10-14 01:28:07 +0200

    Explication ChatMD à l'ouverture

--- chatdata.js
+++ chatdata.js
@@ -3,10 +3,69 @@ const defaultMessage = "Désolé, je ne comprends pas votre question.";
 let md = `# ChatMD
 
 > Bonjour, je suis ChatMD, un chatbot, que vous pouvez configurer par vous-même en Markdown :
-> 1. Créez un fichier en markdown et mettez-le en ligne : sur CodiMD, ou sur une forge
-> 2. Respectez la syntaxe de ChatMD pour définir votre chatbot
 > 
-> Votre chatbot est prêt et visible à l'adresse suivante : [https://eyssette.github.io/chatMD/#URL](https://eyssette.github.io/chatMD/#URL) (Mettez l'url de votre fichier à la place de URL)
+> - Créez un fichier en markdown et mettez-le en ligne : sur CodiMD, ou sur une forge
+> - Respectez la syntaxe de ChatMD pour définir votre chatbot
+> 
+> Votre chatbot est alors prêt et visible à l'adresse suivante : [https://eyssette.github.io/chatMD/#URL](https://eyssette.github.io/chatMD/#URL) (Mettez l'url de votre fichier à la place de URL)
+> 
+> 1. [Qu'est-ce que le markdown ?](Markdown)
+> 2. [CodiMD, une forge : qu'est-ce que c'est ?](CodiMD et forge)
+> 3. [Quelle syntaxe faut-il respecter pour ChatMD ?](Syntaxe)
+> 4. [Tu peux me donner un exemple !](Exemple)
+
+## Markdown
+- markdown
+Le markdown est un format de balisage très léger qui permet d'écrire rapidement du texte formaté.
+
+Pour découvrir le Markown, vous pouvez suivre ce [tutoriel](https://www.markdowntutorial.com/fr/).
+
+## CodiMD et forge
+- codimd
+- forge
+- en ligne
+
+[CodiMD](https://codimd.apps.education.fr/) est un outil pour écrire du markdown en ligne et il est disponible avec vos identifiants académiques sur le [portail Apps Edu](https://portail.apps.education.fr/).
+
+Une forge est un outil plus complet qui permet d'héberger des fichiers texte et de les transformer en site web, en carte mentale, ou encore ici en chatbot ! ChatMD est présent sur la [Forge des Communs Numériques](https://forge.aeif.fr/) et vous pouvez aussi mettre vos fichiers sur cette forge.
+
+## Syntaxe
+- syntaxe
+
+La syntaxe pour écrire un chatbot avec chatMD est la suivante : [voir un exemple](#Exemple)
+
+- On définit le titre du chatbot dans un titre de niveau 1
+- Le message initial est à mettre dans un bloc de citation après le titre du chatbot
+- Les titres de niveau 2 servent à identifier les réponses possibles du chatbot
+- Sous chaque titre de niveau 2 : 
+	- On indique avec une liste non ordonnée les mots clés ou expressions qui vont déclencher la réponse. On peut éventuellement s'en passer si on guide l'utilisateur avec un choix d'options (voir ci-dessous).
+	- On écrit une réponse en Markdown.
+	- [Optionnel] On indique avec une liste ordonnée les options possibles. Chaque élément de la liste doit être un lien en Mardown de la forme suivante : \`[intitulé de l'option](identifiant de l'option, qui doit correspondre à l'un des titres de niveau 2)\`.
+
+## Exemple
+- exemple
+- donner un exemple
+- concret
+- concrètement
+
+Voici un exemple de chatbot qui a été créé avec ChatMD à partir du travail de Guillaume Berthelot et de Jérémy Navoizat : 
+
+- [Utilisation d'un microscope](https://eyssette.github.io/chatMD/#https://codimd.apps.education.fr/xGNHIJSeTVCk6FHas-_71g/download).
+
+Vous pouvez aussi [voir la source](https://codimd.apps.education.fr/xGNHIJSeTVCk6FHas-_71g?both) pour mieux comprendre
+
+## Merci
+- merci
+- remercier
+- félicitation
+- bravo
+- super
+- excellent
+- génial
+
+Merci ! Si vous aimez ce travail, vous aimerez peut-être aussi les autres outils ou sites que je propose sur [mon site perso](https://eyssette.github.io).
+
+
 `;
 
 let chatData;
