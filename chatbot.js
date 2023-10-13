9abf54cbe363c9b12d753fc640cbb0d5ed0f391b
eyssette <cedric.eyssette@gmail.com>
2023-10-13 14:00:42 +0200

    Premier essai data en markdown

--- chatbot.js
+++ chatbot.js
@@ -1,135 +1,124 @@
-const chatData = [
-	["discussion1", ["hello", "you"], ["Hello how are you ?", "Good ?"], null],
-	[
-		"discussion2",
-		["salut", "toi"],
-		["Salut ça va", "Nickel ?"],
-		["option 1", "option 2"],
-	],
-	["Nom du chatbot"],
-];
+document.addEventListener("DOMContentLoaded", function () {
+	const chatbotName = chatData.pop();
+	document.getElementById("chatbot-name").textContent = chatbotName;
 
-const initialMessage = "Bonjour, en quoi puis-je vous être utile ?";
+	const chatContainer = document.getElementById("chat");
+	const userInput = document.getElementById("user-input");
+	const sendButton = document.getElementById("send-button");
 
-const chatbotName = chatData.pop();
-document.getElementById("chatbot-name").textContent = chatbotName;
-
-const chatContainer = document.getElementById("chat");
-const userInput = document.getElementById("user-input");
-const sendButton = document.getElementById("send-button");
-
-function createChatMessage(message, isUser) {
-	const chatMessage = document.createElement("div");
-	chatMessage.classList.add("message");
-	chatMessage.classList.add(isUser ? "user-message" : "bot-message");
-	chatMessage.textContent = message;
-	chatContainer.appendChild(chatMessage);
-}
-
-function levenshteinDistance(a, b) {
-	if (a.length === 0) return b.length;
-	if (b.length === 0) return a.length;
-
-	const matrix = [];
-	for (let i = 0; i <= b.length; i++) {
-		matrix[i] = [i];
+	function createChatMessage(message, isUser) {
+		const chatMessage = document.createElement("div");
+		chatMessage.classList.add("message");
+		chatMessage.classList.add(isUser ? "user-message" : "bot-message");
+		chatMessage.textContent = message;
+		chatContainer.appendChild(chatMessage);
 	}
 
-	for (let j = 0; j <= a.length; j++) {
-		matrix[0][j] = j;
-	}
+	function levenshteinDistance(a, b) {
+		if (a.length === 0) return b.length;
+		if (b.length === 0) return a.length;
 
-	for (let i = 1; i <= b.length; i++) {
-		for (let j = 1; j <= a.length; j++) {
-			const cost = a[j - 1] === b[i - 1] ? 0 : 1;
-			matrix[i][j] = Math.min(
-				matrix[i - 1][j] + 1,
-				matrix[i][j - 1] + 1,
-				matrix[i - 1][j - 1] + cost
-			);
+		const matrix = [];
+		for (let i = 0; i <= b.length; i++) {
+			matrix[i] = [i];
 		}
+
+		for (let j = 0; j <= a.length; j++) {
+			matrix[0][j] = j;
+		}
+
+		for (let i = 1; i <= b.length; i++) {
+			for (let j = 1; j <= a.length; j++) {
+				const cost = a[j - 1] === b[i - 1] ? 0 : 1;
+				matrix[i][j] = Math.min(
+					matrix[i - 1][j] + 1,
+					matrix[i][j - 1] + 1,
+					matrix[i - 1][j - 1] + cost
+				);
+			}
+		}
+
+		return matrix[b.length][a.length];
 	}
 
-	return matrix[b.length][a.length];
-}
+	const LEVENSHTEIN_THRESHOLD = 3; // Seuil de similarité
+	const MATCH_SCORE_IDENTITY = 5; // Pour régler le fait de privilégier l'identité d'un mot à la simple similarité
 
-const LEVENSHTEIN_THRESHOLD = 3; // Seuil de similarité
-const MATCH_SCORE_IDENTITY = 5; // Pour régler le fait de privilégier l'identité d'un mot à la simple similarité
+	function chatbotResponse(userInputText) {
+		let bestMatch = null;
+		let bestMatchScore = 0;
+		let bestDistanceScore = 0;
+		let userInputTextToLowerCase = userInputText.toLowerCase();
 
-function chatbotResponse(userInputText) {
-	let bestMatch = null;
-	let bestMatchScore = 0;
-	let bestDistanceScore = 0;
-	let userInputTextToLowerCase = userInputText.toLowerCase();
-
-	for (let i = 0; i < chatData.length; i++) {
-		const keywords = chatData[i][1];
-		const responses = chatData[i][2];
-		let matchScore = 0;
-		let distanceScore = 0;
-		let distance = 0;
-		for (let keyword of keywords) {
-			let keywordToLowerCase=keyword.toLowerCase();
-			if (userInputTextToLowerCase.includes(keywordToLowerCase)) {
-				matchScore = matchScore + MATCH_SCORE_IDENTITY;
-			} else {
-				distance = levenshteinDistance(keywordToLowerCase, userInputTextToLowerCase);
-				if (distance < LEVENSHTEIN_THRESHOLD) {
-					distanceScore++;
+		for (let i = 0; i < chatData.length; i++) {
+			const keywords = chatData[i][1];
+			const responses = chatData[i][2];
+			let matchScore = 0;
+			let distanceScore = 0;
+			let distance = 0;
+			for (let keyword of keywords) {
+				let keywordToLowerCase = keyword.toLowerCase();
+				if (userInputTextToLowerCase.includes(keywordToLowerCase)) {
+					matchScore = matchScore + MATCH_SCORE_IDENTITY;
+				} else {
+					distance = levenshteinDistance(
+						keywordToLowerCase,
+						userInputTextToLowerCase
+					);
+					if (distance < LEVENSHTEIN_THRESHOLD) {
+						distanceScore++;
+					}
 				}
 			}
-		}
-		/* console.log("matchScore:"+matchScore);
+			/* console.log("matchScore:"+matchScore);
 		console.log("distanceScore:"+distanceScore); */
-		if (matchScore == 0) {
-			if (distanceScore > bestDistanceScore) {
-				matchScore++;
-				bestDistanceScore = distanceScore;
+			if (matchScore == 0) {
+				if (distanceScore > bestDistanceScore) {
+					matchScore++;
+					bestDistanceScore = distanceScore;
+				}
+			}
+			if (matchScore > bestMatchScore) {
+				bestMatch = responses;
+				bestMatchScore = matchScore;
 			}
 		}
-		if (matchScore > bestMatchScore) {
-			bestMatch = responses;
-			bestMatchScore = matchScore;
+
+		if (bestMatch) {
+				const response = Array.isArray(bestMatch) ? bestMatch.join('\n') : bestMatch;
+				createChatMessage(response, false);
+		} else {
+			// Aucune correspondance trouvée
+			createChatMessage(defaultMessage, false);
 		}
 	}
 
-	if (bestMatch) {
-		for (let response of bestMatch) {
-			createChatMessage(response, false);
+	sendButton.addEventListener("click", () => {
+		const userInputText = userInput.innerText;
+		if (userInputText.trim() !== "") {
+			createChatMessage(userInputText, true);
+			chatbotResponse(userInputText);
+			userInput.innerText = "";
 		}
-	} else {
-		// Aucune correspondance trouvée
-		createChatMessage("Désolé, je ne comprends pas votre question.", false);
-	}
-}
+	});
 
-sendButton.addEventListener("click", () => {
-	const userInputText = userInput.innerText;
-	if (userInputText.trim() !== "") {
-		createChatMessage(userInputText, true);
-		chatbotResponse(userInputText);
-		userInput.innerText = "";
-	}
+	userInput.addEventListener("keyup", (event) => {
+		if (event.key === "Enter") {
+			sendButton.click();
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+	});
+
+	userInput.focus();
+
+	userInput.addEventListener("focus", function () {
+		this.classList.remove("placeholder");
+	});
+
+	userInput.addEventListener("blur", function () {
+		this.classList.add("placeholder");
+	});
+
+	// Message d'accueil du chatbot
+	createChatMessage(initialMessage, false);
 });
-
-userInput.addEventListener("keyup", (event) => {
-	if (event.key === "Enter") {
-		sendButton.click();
-		window.scrollTo(0, document.body.scrollHeight);
-	}
-});
-
-userInput.focus();
-
-userInput.addEventListener('focus', function(){
-  this.classList.remove('placeholder');
-});
-
-userInput.addEventListener('blur', function(){
-	this.classList.add('placeholder');
-});
-
-
-
-// Message d'accueil du chatbot
-createChatMessage(initialMessage, false);
---
+++ chatdata.js
@@ -0,0 +1,83 @@
+/* const chatData = [
+	["discussion1", ["ce qu'est la philosophie", "défini", "philosophie"], ["La philosophie c'est l'art de se poser des questions"], null],
+	[
+		"discussion2",
+		["démarches","philosophie"],
+		["Salut ça va", "Nickel ?"],
+		["option 1", "option 2"],
+	],
+	["Nom du chatbot"],
+]; */
+
+const initialMessage = "Bonjour, en quoi puis-je vous être utile ?";
+const defaultMessage = "Désolé, je ne comprends pas votre question.";
+
+md = `# Chatbot Philo
+
+## Définition de la philosophie
+- défini
+- ce qu'est la philosophie
+
+Faire de la philosophie, c'est réfléchir à des questions qui portent sur des notions générales et fondamentales de notre existence (la liberté, le bien et le mal, la vérité, …).
+
+## Les grandes démarches
+- démarche
+
+En philosophie, il y a 4 démarches importantes :
+- problématiser
+- analyser
+- argumenter
+- mobiliser ses connaissances
+
+1. option 1
+2. option 2
+`
+
+const chatData = parseMarkdown(md);
+
+/* console.log(chatData); */
+
+function parseMarkdown(markdownContent) {
+    const lines = markdownContent.split('\n');
+    let chatbotData = [];
+    let chatbotTitle = [''];
+    let currentH2Title = null;
+    let currentLiItems = [];
+    let content = []
+    let lastOrderedList = null;
+    const regexOrderedList = /^\d{1,3}\.\s/;
+    let listParsed = false;
+
+    for (let line of lines) {
+        if (line.startsWith("# ")) {
+        		chatbotTitle[0] = line.replace("# ", "").trim()
+        } else if (line.startsWith("## ")) {
+            if (currentH2Title) {
+                chatbotData.push([currentH2Title, currentLiItems, content, lastOrderedList]);
+            }
+            currentH2Title = line.replace("## ", "").trim(); // Titre h2
+            currentLiItems = [];
+            lastOrderedList = null;
+            listParsed = false;
+            content = [];
+        } else if (line.startsWith("- ") && !listParsed) {
+            currentLiItems.push(line.replace("- ", "").trim());
+        } else if (line.match(regexOrderedList)) {
+		        listParsed = false;
+            if (!lastOrderedList) {
+                lastOrderedList = [];
+            }
+            lastOrderedList.push(line.replace(/^\d+\.\s/, "").trim());
+        } else {
+        		if (line.length>0) {
+            	content.push(line);
+              listParsed = true;
+           	}
+        }
+    }
+    chatbotData.push([currentH2Title, currentLiItems, content.join('\n'), lastOrderedList])
+
+	chatbotData.push(chatbotTitle); 
+
+    return chatbotData;
+}
