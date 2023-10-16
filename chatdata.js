9fd87b3eb52ecaaf5700b20c3aa552b62faabb6b
eyssette <cedric.eyssette@gmail.com>
2023-10-16 14:44:11 +0200

    Ajout de commentaires

--- chatdata.js
+++ chatdata.js
@@ -1,23 +1,23 @@
-const defaultMessage =  [
-"Désolé, je ne comprends pas votre question.",
-"Pardonnez-moi, mais je ne saisis pas votre demande.",
-"Excusez-moi, je ne parviens pas à comprendre ce que vous demandez.",
-"Je suis navré, mais je ne parviens pas à saisir votre question.",
-"Malheureusement, je ne suis pas en mesure de comprendre votre question.",
-"Je suis désolé, mais je ne saisis pas votre question.",
-"Pardonnez-moi, mais je ne saisis pas le sens de votre question.",
-"Je m'excuse, mais je ne parviens pas à saisir votre demande. Pouvez-vous reformuler votre question, s'il vous plaît ?",
-"Je ne suis pas sûr de comprendre ce que vous demandez. Pouvez-vous expliquer davantage ?",
-"Je ne peux pas répondre à votre question telle qu'elle est formulée. Pouvez-vous la poser différemment ?",
-"Votre question ne semble pas correspondre à mes capacités actuelles. Pourriez-vous la reformuler autrement ?",
-"Je n'ai malheureusement pas compris votre requête.",
-"Je suis désolé, je ne suis pas capable de répondre.",
-"Malheureusement, je ne peux pas répondre à votre question.",
-"Malheureusement je n'arrive pas à comprendre votre requête.",
-"Excusez-moi, je ne comprends pas votre requête.",
-"Excusez-moi, je n'arrive pas à répondre à votre question.",
-"Je ne parviens pas à répondre à votre requête. Veuillez m'excuser."
-]
+const defaultMessage = [
+	"Désolé, je ne comprends pas votre question.",
+	"Pardonnez-moi, mais je ne saisis pas votre demande.",
+	"Excusez-moi, je ne parviens pas à comprendre ce que vous demandez.",
+	"Je suis navré, mais je ne parviens pas à saisir votre question.",
+	"Malheureusement, je ne suis pas en mesure de comprendre votre question.",
+	"Je suis désolé, mais je ne saisis pas votre question.",
+	"Pardonnez-moi, mais je ne saisis pas le sens de votre question.",
+	"Je m'excuse, mais je ne parviens pas à saisir votre demande. Pouvez-vous reformuler votre question, s'il vous plaît ?",
+	"Je ne suis pas sûr de comprendre ce que vous demandez. Pouvez-vous expliquer davantage ?",
+	"Je ne peux pas répondre à votre question telle qu'elle est formulée. Pouvez-vous la poser différemment ?",
+	"Votre question ne semble pas correspondre à mes capacités actuelles. Pourriez-vous la reformuler autrement ?",
+	"Je n'ai malheureusement pas compris votre requête.",
+	"Je suis désolé, je ne suis pas capable de répondre.",
+	"Malheureusement, je ne peux pas répondre à votre question.",
+	"Malheureusement je n'arrive pas à comprendre votre requête.",
+	"Excusez-moi, je ne comprends pas votre requête.",
+	"Excusez-moi, je n'arrive pas à répondre à votre question.",
+	"Je ne parviens pas à répondre à votre requête. Veuillez m'excuser.",
+];
 
 let md = `# ChatMD
 
@@ -122,15 +122,27 @@ Merci ! Si vous aimez ce travail, vous aimerez peut-être aussi les autres outil
 let chatData;
 
 function getMarkdownContent() {
+	// Récupération du markdown externe
 	let urlMD = window.location.hash.substring(1); // Récupère l'URL du hashtag sans le #
 	if (urlMD !== "") {
-		if (urlMD.startsWith('https://github.com')) {
-			urlMD = urlMD.replace('https://github.com', 'https://raw.githubusercontent.com');
-				urlMD = urlMD.replace('/blob/', '/');
-			}
-			if (urlMD.startsWith('https://codimd') && urlMD.indexOf('download')=== -1 ) {
-				urlMD = urlMD.replace('?edit','').replace('?both','').replace('?view','')+'/download';
-			}
+		// Gestion des fichiers hébergés sur github
+		if (urlMD.startsWith("https://github.com")) {
+			urlMD = urlMD.replace(
+				"https://github.com",
+				"https://raw.githubusercontent.com"
+			);
+			urlMD = urlMD.replace("/blob/", "/");
+		}
+		// Gestion des fichiers hébergés sur codiMD
+		if (
+			urlMD.startsWith("https://codimd") &&
+			urlMD.indexOf("download") === -1
+		) {
+			urlMD =
+				urlMD.replace("?edit", "").replace("?both", "").replace("?view", "") +
+				"/download";
+		}
+		// Récupération du contenu du fichier
 		fetch(urlMD)
 			.then((response) => response.text())
 			.then((data) => {
@@ -151,7 +163,7 @@ function parseMarkdown(markdownContent) {
 	const lines = markdownContent.split("\n");
 	let chatbotData = [];
 	let chatbotTitle = [""];
-	
+
 	let initialMessageComputed = false;
 	let currentH2Title = null;
 	let currentLiItems = [];
@@ -163,20 +175,25 @@ function parseMarkdown(markdownContent) {
 	let initialMessageOptions = [];
 
 	for (let line of lines) {
+		// On parcourt le contenu du fichier ligne par ligne
 		if (line.startsWith("# ")) {
+			// Récupération du titre du chatbot
 			chatbotTitle[0] = line.replace("# ", "").trim();
 		} else if (line.startsWith("> ") && !initialMessageComputed) {
+			// Récupération du message initial du chatbot, défini par un bloc citation
 			line = line.replace(/^> /, "").trim();
 			if (line.match(regexOrderedList)) {
+				// Récupération des options dans le message initial, s'il y en a
 				line = line.replace(/^> /, "").trim();
 				const listContent = line.replace(/^\d+\.\s/, "").trim();
 				const link = listContent.replace(/^\[.*?\]\(/, "").replace(/\)$/, "");
 				const text = listContent.replace(/\]\(.*/, "").replace(/^\[/, "");
 				initialMessageOptions.push([text, link]);
 			} else {
-				initialMessageContent.push(line)
+				initialMessageContent.push(line);
 			}
 		} else if (line.startsWith("## ")) {
+			// Gestion des identifiants de réponse, et début de traitement du contenu de chaque réponse
 			initialMessageComputed = true;
 			if (currentH2Title) {
 				chatbotData.push([
@@ -192,8 +209,10 @@ function parseMarkdown(markdownContent) {
 			listParsed = false;
 			content = [];
 		} else if (line.startsWith("- ") && !listParsed) {
+			// Gestion des listes
 			currentLiItems.push(line.replace("- ", "").trim());
 		} else if (line.match(regexOrderedList)) {
+			// Cas des listes ordonnées
 			listParsed = false;
 			if (!lastOrderedList) {
 				lastOrderedList = [];
@@ -204,7 +223,8 @@ function parseMarkdown(markdownContent) {
 			lastOrderedList.push([text, link]);
 			/* lastOrderedList.push(listContent); */
 		} else if (line.length > 0) {
-			content.push(line+'\n');
+			// Gestion du reste du contenu
+			content.push(line + "\n");
 			listParsed = true;
 		}
 	}
@@ -215,7 +235,7 @@ function parseMarkdown(markdownContent) {
 		lastOrderedList,
 	]);
 
-	const initialMessage = [initialMessageContent,initialMessageOptions];
+	const initialMessage = [initialMessageContent, initialMessageOptions];
 	chatbotData.push(initialMessage);
 	chatbotData.push(chatbotTitle);
 
