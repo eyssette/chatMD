694e5ffc5be489ec18d2151c6a1ac2cc354d5605
Eyssette <cedric.eyssette@gmail.com>
2024-01-31 06:51:54 +0100

    bug Autoscroll écrans tactiles

--- chatbot.js
+++ chatbot.js
@@ -146,7 +146,7 @@ function createChatBot(chatData) {
 					});
 					document.addEventListener('touchstart', function () {
 						// On remet le scroll automatique si on scrolle vers le bas de la page
-						if(window.scrollY + window.innerHeight + 100 >= document.documentElement.scrollHeight) {
+						if(window.scrollY + window.innerHeight + 50 >= document.documentElement.scrollHeight) {
 							enableAutoScroll();
 						} else {
 							mutationObserver.disconnect();
