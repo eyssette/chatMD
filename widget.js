36f4df32b060b57e0b696028f8b6b7969555c39e
eyssette <cedric.eyssette@gmail.com>
2024-05-01 13:50:30 +0200

    CSS pour le widget : petits écrans

--- widget.js
+++ widget.js
@@ -33,6 +33,24 @@ const widgetStyle = `
 	background: linear-gradient(90deg, rgb(239, 242, 247) 0%, 7.60286%, rgb(237, 240, 249) 15.2057%, 20.7513%, rgb(235, 239, 248) 26.297%, 27.6386%, rgb(235, 239, 248) 28.9803%, 38.2826%, rgb(231, 237, 249) 47.585%, 48.1216%, rgb(230, 236, 250) 48.6583%, 53.1306%, rgb(228, 236, 249) 57.6029%, 61.5385%, rgb(227, 234, 250) 65.4741%, 68.7835%, rgb(222, 234, 250) 72.093%, 75.7603%, rgb(219, 230, 248) 79.4275%, 82.8265%, rgb(216, 229, 248) 86.2254%, 87.8354%, rgb(213, 228, 249) 89.4454%, 91.8605%, rgb(210, 226, 249) 94.2755%, 95.4383%, rgb(209, 225, 248) 96.6011%, 98.3005%, rgb(208, 224, 247) 100%);
 }
 
+@media screen and (max-width: 500px) {
+	#chatmdWidget {
+		bottom: 10px;
+		right: 10px;
+	}
+	#chatmdWidget div:nth-of-type(2) {
+		right: 0px;
+		padding: 0px;
+		width: 100%;
+		top: 0px;
+		bottom: auto;
+		height: 500px;
+	}
+	#chatmdWidget div:nth-of-type(2) iframe {
+		width: 100%;
+		height: 540px;
+	}
+}
 `;
 
 const styleElement = document.createElement("style");
