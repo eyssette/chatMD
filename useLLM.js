97f86ce1276cef0420080fd2d98c8be6f2afd002
Eyssette <cedric.eyssette@gmail.com>
2024-05-27 19:28:14 +0200

    gestion LLM avec Cohere

--- useLLM.js
+++ useLLM.js
@@ -7,6 +7,23 @@ const chatContainerElement = document.getElementById("chat");
 //getAnswerFromLLM(userPrompt);
 
 // Une fonction pour lire la réponse streamée du LLM
+async function readStreamCohere(streamableObject,id, chatMessage) {
+	answerFromLLM[id] = '';
+	let chunkMessage = "";
+	for await (const chunk of streamableObject) {
+		const chunkString = new TextDecoder().decode(chunk)
+		const chunkArray = chunkString.trim().split('\n');
+		chunkArray.forEach(chunkElement => {
+			const chunkObject = JSON.parse(chunkElement.trim());
+			if (chunkObject.event_type == 'text-generation') {
+				chunkMessage = chunkObject.text
+				chatMessage.innerHTML = chatMessage.innerHTML + chunkObject.text
+			}
+		});
+		window.scrollTo(0, document.body.scrollHeight);
+	}
+}
+
 async function readStream(streamableObject, id) {
 	const reader = streamableObject.getReader();
 	const decoder = new TextDecoder();
@@ -63,37 +80,46 @@ document.body.addEventListener("keypress", (event) => {
 	}
 })
 
+let bodyObject = {
+	model: yamlUseLLMmodel,
+	stream: true,
+	max_tokens: yamlUseLLMmaxTokens,
+	frequency_penalty: 0,
+	presence_penalty: 0,
+	temperature: 0.7,
+	top_p: 0.95,
+}
+
 // Fonction pour récupérer une réponse d'un LLM à partir d'un prompt
 function getAnswerFromLLM(userPrompt, informations) {
 	idAnswer++;
 	if (informations.length>0) {
 		informations = yamlUseLLMragPrompt+informations
 	}
+	const isCohere = yamlUseLLMurl.includes('cohere');
+
+	if (isCohere) {
+		bodyObject.message = yamlUseLLMpreprompt+userPrompt+yamlUseLLMpostprompt+informations;
+	} else {
+		bodyObject.messages = [
+			{
+				content: yamlUseLLMsystemPrompt,
+				role: "system",
+			},
+			{
+				content: yamlUseLLMpreprompt+userPrompt+yamlUseLLMpostprompt+informations,
+				role: "user",
+			},
+		]
+	}
+
 	fetch(yamlUseLLMurl, {
 		method: "POST",
 		headers: {
 			"Authorization": "Bearer "+yamlUseLLMapiKey,
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({
-			messages: [
-				{
-					content: yamlUseLLMsystemPrompt,
-					role: "system",
-				},
-				{
-					content: yamlUseLLMpreprompt+userPrompt+yamlUseLLMpostprompt+informations,
-					role: "user",
-				},
-			],
-			model: yamlUseLLMmodel,
-			stream: true,
-			max_tokens: yamlUseLLMmaxTokens,
-			frequency_penalty: 0,
-			presence_penalty: 0,
-			temperature: 0.7,
-			top_p: 0.95,
-		}),
+		body: JSON.stringify(bodyObject),
 	})
 		.then((response) => {
 			LLMactive = true;
@@ -101,7 +127,9 @@ function getAnswerFromLLM(userPrompt, informations) {
 			chatMessage.classList.add("message");
 			chatMessage.classList.add("bot-message");
 			chatContainerElement.appendChild(chatMessage);
-			readStream(response.body,idAnswer);
+			if (isCohere) {
+				readStreamCohere(response.body,idAnswer, chatMessage);
+			} else {
 			const intervalId = setInterval(() => {
 				if (LLMactive) {
 					chatMessage.innerHTML = answerFromLLM[idAnswer];
@@ -110,6 +138,7 @@ function getAnswerFromLLM(userPrompt, informations) {
 					clearInterval(intervalId);
 				}
 			}, 50);
+			}
 		})
 		.catch((error) => {
 			console.error("Erreur:", error.message);
