49a2262e76af4aca3184ed646e909cb2da83a599
Eyssette <cedric.eyssette@gmail.com>
2024-07-02 00:02:21 +0200

    fonction typewriter à part

--- combine-scripts.py
+++ combine-scripts.py
@@ -1,6 +1,7 @@
 # Chemin des fichiers source
 custom_js_file = "custom.js"
 utils_js_file = "utils.js"
+typewriter_js_file = "typewriter.js"
 chatbot_js_file = "chatbot.js"
 chatdata_js_file = "chatdata.js"
 
@@ -8,14 +9,15 @@ chatdata_js_file = "chatdata.js"
 output_file = "script.min.js"
 
 # Lire le contenu des fichiers source
-with open(chatbot_js_file, "r") as chatbot_file, open(chatdata_js_file, "r") as chatdata_file, open(custom_js_file, "r") as custom_file, open(utils_js_file, "r") as utils_file:
+with open(chatbot_js_file, "r") as chatbot_file, open(chatdata_js_file, "r") as chatdata_file, open(custom_js_file, "r") as custom_file, open(utils_js_file, "r") as utils_file, open(typewriter_js_file, "r") as typewriter_file:
     custom_content = custom_file.read()
     utils_content = utils_file.read()
+    typewriter_content = typewriter_file.read()
     chatbot_content = chatbot_file.read()
     chatdata_content = chatdata_file.read()
 
 # Concaténer le contenu des fichiers source
-combined_content = custom_content + utils_content + chatbot_content + chatdata_content
+combined_content = custom_content + utils_content + typewriter_content + chatbot_content + chatdata_content
 
 # Écrire le contenu minifié dans le fichier de sortie
 with open(output_file, "w") as output:
---
+++ typewriter.js
@@ -0,0 +1,162 @@
+
+const chatContainer = document.getElementById("chat");
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
+
+	// Le focus automatique sur l'userInput est désactivé sur les téléphones mobiles
+	const isMobile =
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		);
+	const autoFocus = isMobile ? false : true;
+
+	let typed;
+	const pauseTypeWriter = "^300 ";
+	const stopTypeWriterExecutionTimeThreshold = 800;
+	// Effet machine à écrire
+	function typeWriter(content, element) {
+		// Gestion de "Enter" pour stopper l'effet machine à écrire
+		const messageTypeEnterToStopTypeWriter =
+			window.innerWidth > 880
+				? "Appuyez sur “Enter” pour stopper l'effet “machine à écrire” et afficher la réponse immédiatement"
+				: "“Enter” pour stopper l'effet “machine à écrire”";
+		function stopTypeWriter(slowContent) {
+			typed.stop();
+			typed.reset();
+			// Cas du Latex
+			slowContent = slowContent.replaceAll(/`(<span class="katex-mathml">(.|\n)*?<\/span>)`/gm,'$1')
+			slowContent = slowContent.replaceAll(/`(<span class=".?strut">.*?<\/span>)`/g,'$1')
+			// On doit conserver les retours à la ligne dans les blocs "pre"
+			const contentKeepReturnInCode = slowContent.replaceAll(/(<pre(.|\n)*<\/pre>)/gm,function(match){
+				return match.replaceAll('\n','RETURNCHARACTER')
+			})
+			const contentArray = contentKeepReturnInCode.split("\n");
+			// On découpe chaque paragraphe pour pouvoir ensuite l'afficher d'un coup
+			const contentArrayFiltered = contentArray.map((element) =>
+				element.startsWith(pauseTypeWriter)
+					? element.replace(pauseTypeWriter, "").replaceAll('RETURNCHARACTER','\n') + "`"
+					: element.endsWith("`")
+					? "`" + element.replaceAll('RETURNCHARACTER','\n')
+					: "`" + element.replaceAll('RETURNCHARACTER','\n') + "`"
+			);
+			typed.strings = [contentArrayFiltered.join(" ")];
+			typed.start();
+			typed.destroy();
+		}
+		let mutationObserver;
+		function keypressHandler(event) {
+			if (event.key === "Enter") {
+				mutationObserver.disconnect();
+				observerConnected = false;
+				stopTypeWriter(content);
+			}
+		}
+
+		let counter = 0;
+		const start = Date.now();
+		let observerConnected = true;
+		function handleMutation() {
+			// On arrête l'effet “machine à écrire” si le temps d'exécution est trop important
+			const executionTime = Date.now() - start;
+			if (
+				counter == 50 &&
+				executionTime > stopTypeWriterExecutionTimeThreshold &&
+				observerConnected
+			) {
+				stopTypeWriter(content);
+				observerConnected = false;
+				mutationObserver.disconnect();
+			}
+			// On scrolle automatiquement la fenêtre pour suivre l'affichage du texte
+			scrollWindow();
+			counter++;
+		}
+
+		// Configuration de MutationObserver
+		const observerConfig = {
+			childList: true,
+			subtree: true,
+		};
+
+		// S'il y a des options en fin de message, on les fait apparaître d'un coup, sans effet typeWriter
+		content = content.replace(
+			/(<ul class="messageOptions"\>[\s\S]*<\/ul>)/gm, pauseTypeWriter + "`$1`");
+
+		// Effet machine à écrire
+		typed = new Typed(element, {
+			strings: [content],
+			typeSpeed: -5000,
+			startDelay: 100,
+			showCursor: false,
+			onBegin: () => {
+				// Quand l'effet démarre, on refocalise sur userInput (sauf sur les smartphones)
+				if (autoFocus) {
+					userInput.focus();
+				}
+				// On détecte un appui sur Enter pour stopper l'effet machine à écrire
+				userInput.addEventListener("keypress", keypressHandler);
+				userInput.setAttribute("placeholder", messageTypeEnterToStopTypeWriter);
+
+				// On détecte le remplissage petit à petit du DOM pour scroller automatiquement la fenêtre vers le bas
+				mutationObserver = new MutationObserver(handleMutation);
+				function enableAutoScroll() {
+					mutationObserver.observe(chatContainer, observerConfig);
+				}
+				enableAutoScroll();
+
+				setTimeout(() => {
+					// Arrêter le scroll automatique en cas de mouvement de la souris ou de contact avec l'écran
+					document.addEventListener("mousemove", function () {
+						observerConnected = false;
+						mutationObserver.disconnect();
+					});
+					document.addEventListener("wheel", function (e) {
+						// On remet le scroll automatique si on scrolle vers le bas de la page
+						if (e.deltaY > 0) {
+							// On détecte si on a fait un mouvement vers le bas
+							if (
+								window.scrollY + window.innerHeight >=
+								document.body.offsetHeight
+							) {
+								enableAutoScroll();
+							} else {
+								observerConnected = false;
+								mutationObserver.disconnect();
+							}
+						} else {
+							observerConnected = false;
+							mutationObserver.disconnect();
+						}
+					});
+					document.addEventListener("touchstart", function () {
+						observerConnected = false;
+						mutationObserver.disconnect();
+						// On remet le scroll automatique si on scrolle vers le bas de la page
+						setTimeout(() => {
+							if (
+								window.scrollY + window.innerHeight + 200 >=
+								document.documentElement.scrollHeight
+							) {
+								enableAutoScroll();
+							}
+						}, 5000);
+					});
+				}, 1000);
+			},
+			onComplete: () => {
+				// Gestion de textFit pour les éléments en Latex
+				if(yamlUseAddOns && yamlUseAddOns.includes("textFit")) {
+					textFit(element.querySelectorAll('.katex-display'))
+				}
+				// Quand l'effet s'arrête on supprime la détection du bouton Enter pour stopper l'effet
+				userInput.removeEventListener("keypress", keypressHandler);
+				if (
+					userInput.getAttribute("placeholder") ==
+					messageTypeEnterToStopTypeWriter
+				) {
+					userInput.setAttribute("placeholder", "Écrivez votre message");
+				}
+				mutationObserver.disconnect();
+			},
+		});
+	}
