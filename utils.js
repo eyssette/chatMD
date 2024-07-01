c8c8e2e3b6baf316c0bc60550d8414c1a5ace45c
Eyssette <cedric.eyssette@gmail.com>
2024-07-01 23:23:31 +0200

    preprocess variables : suppression variable ssi lastMatch

--- utils.js
+++ utils.js
@@ -7,12 +7,13 @@ function getRandomElement(array) {
 function processVariables(content, preprocess = false) {
 	// Les variables fixes qui commencent par _ sont traitées avant de parser le contenu du Markdown
 	const regex = preprocess ? /@{(_\S+)}/g : /@{(\S+)}/g
-	return content.replaceAll(regex, function (match, variableName) {
+	return content.replaceAll(regex, function (match, variableName,positionMatch) {
+		const positionLastMatch = content.lastIndexOf(match)
 		if (yamlData && yamlData.variables && yamlData.variables[variableName]) {
 			const variableValue = yamlData.variables[variableName];
 			const variableValueSplit = variableValue.split("///");
 			const variableValueChoice = getRandomElement(variableValueSplit);
-			if(preprocess) {
+			if(preprocess && positionMatch == positionLastMatch) {
 				// Les variables fixes qui ont été traitées au tout début, avant de parser le contenu du Markdown, sont ensuite supprimés.
 				delete yamlData.variables[variableName];
 			}
