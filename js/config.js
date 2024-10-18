924c90047e3eeee562d3bcc5ba124c466346f63e
Eyssette <cedric.eyssette@gmail.com>
2024-10-18 16:55:24 +0200

    shortcut chatbot orientation

--- js/config.js
+++ js/config.js
@@ -48,6 +48,7 @@ config.shortcuts = [
 		"dissertation-philo",
 		"https://raw.githubusercontent.com/eyssette/chatbot/main/dissertation-philosophie.md",
 	],
+	["orientation", "https://codimd.apps.education.fr/UB1xium-TSqpdHEqdxipTw"],
 	[
 		"multiple-urls",
 		[
