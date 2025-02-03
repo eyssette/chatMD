c632de0203e593820ffc3dc2897820d0d85862ec
eyssette <cedric.eyssette@gmail.com>
2025-02-03 13:33:06 +0100

    Widget: CSS: amélioration padding iframe

--- widget/widget.js
+++ widget/widget.js
@@ -36,9 +36,11 @@ const widgetStyle = `
 	display: none;
 	width: 400px;
 	height: 600px;
-	padding: 10px;
 	border-radius: 15px;
 	background: linear-gradient(90deg, rgb(239, 242, 247) 0%, 7.60286%, rgb(237, 240, 249) 15.2057%, 20.7513%, rgb(235, 239, 248) 26.297%, 27.6386%, rgb(235, 239, 248) 28.9803%, 38.2826%, rgb(231, 237, 249) 47.585%, 48.1216%, rgb(230, 236, 250) 48.6583%, 53.1306%, rgb(228, 236, 249) 57.6029%, 61.5385%, rgb(227, 234, 250) 65.4741%, 68.7835%, rgb(222, 234, 250) 72.093%, 75.7603%, rgb(219, 230, 248) 79.4275%, 82.8265%, rgb(216, 229, 248) 86.2254%, 87.8354%, rgb(213, 228, 249) 89.4454%, 91.8605%, rgb(210, 226, 249) 94.2755%, 95.4383%, rgb(209, 225, 248) 96.6011%, 98.3005%, rgb(208, 224, 247) 100%);
+}
+#chatmdWidget div:nth-of-type(2) iframe {
+	padding: 10px;
 }
 
 @media screen and (max-width: 500px) {
@@ -57,6 +59,7 @@ const widgetStyle = `
 	#chatmdWidget div:nth-of-type(2) iframe {
 		width: 100%;
 		height: 100%;
+		padding:0px;
 	}
 }
 `;
