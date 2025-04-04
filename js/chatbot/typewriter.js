33f33823df4be1701a5c21c55cd8d08add596af4
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-04-04 08:23:25 +0200

    fix: displayMessage seulement s'il y a du contenu à afficher

--- js/chatbot/typewriter.js
+++ js/chatbot/typewriter.js
@@ -297,25 +297,29 @@ function typeWriter(content, element, accelerateFactor) {
 export function displayMessage(html, isUser, chatMessage, container) {
 	return new Promise((resolve) => {
 		// On affiche le message dans un container. Par défaut on affiche le message comme un nouveau message dans le chat, mais on peut définir un container (pour afficher le message comme un élément enfant d'un nouveau message en cas de génération de message à la fois via le markdown et un LLM : dans ce cas, il faut que le contenu des messages se suivent, dans un même message, au lieu d'ajouter un nouveau message à chaque fois)
-		if (container) {
-			chatContainer.appendChild(container);
+		if (html) {
+			if (container) {
+				chatContainer.appendChild(container);
+			} else {
+				container = chatContainer;
+			}
+			container.appendChild(chatMessage);
+			html = isUser ? html : processCopyCode(html);
+			// Effet machine à écrire : seulement quand c'est le chatbot qui répond, sinon affichage direct
+			// Pas d'effet machine à écrire s'il y a la préférence : "prefers-reduced-motion"
+			if (
+				isUser ||
+				window.matchMedia("(prefers-reduced-motion: reduce)").matches ||
+				yaml.typeWriter === false
+			) {
+				// La désactivation de l'effet typewriter avec les backticks n'est plus nécessaire : on les supprime
+				chatMessage.innerHTML = html.replaceAll("`", "");
+				resolve();
+			} else {
+				typeWriter(html, chatMessage).then(() => resolve());
+			}
 		} else {
-			container = chatContainer;
-		}
-		container.appendChild(chatMessage);
-		html = isUser ? html : processCopyCode(html);
-		// Effet machine à écrire : seulement quand c'est le chatbot qui répond, sinon affichage direct
-		// Pas d'effet machine à écrire s'il y a la préférence : "prefers-reduced-motion"
-		if (
-			isUser ||
-			window.matchMedia("(prefers-reduced-motion: reduce)").matches ||
-			yaml.typeWriter === false
-		) {
-			// La désactivation de l'effet typewriter avec les backticks n'est plus nécessaire : on les supprime
-			chatMessage.innerHTML = html.replaceAll("`", "");
 			resolve();
-		} else {
-			typeWriter(html, chatMessage).then(() => resolve());
 		}
 	});
 }
