73aef59091b3f0fc9801c9bbff0d97cdf514dee7
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-04-29 15:11:36 +0200

    chore: configuration ESlint pour les fichiers en .mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-	{ languageOptions: { globals: globals.browser } },
+	{
+		languageOptions: {
+			globals: globals.browser,
+			ecmaVersion: 2018,
+		},
+	},
 	pluginJs.configs.recommended,
 	{
-		"rules": {
-			"semi": ["error", "always"],
-			"indent": ["error", "tab"],
-			"quotes": ["error", "double", { "avoidEscape": true }],
+		files: ["app/**/*.js", "app/**/*.mjs"],
+		rules: {
+			semi: ["error", "always"],
+			indent: "off",
+			quotes: ["error", "double", { avoidEscape: true }],
 			"no-multi-spaces": ["error"],
 			"no-trailing-spaces": ["error"],
 			"comma-spacing": ["error"],
 			"array-bracket-spacing": ["error"],
 			"object-curly-spacing": ["error", "always"],
 			"space-infix-ops": ["error"],
-			"camelcase": ["error"],
+			camelcase: ["error"],
 			"key-spacing": ["error"],
 			"no-duplicate-imports": ["error"],
 			"padded-blocks": ["error", "never"],
 			"space-before-blocks": ["error"],
-			"space-after-keywords": ["error"],
-		}
-	}
+			"keyword-spacing": ["error"],
+		},
+	},
 ];
