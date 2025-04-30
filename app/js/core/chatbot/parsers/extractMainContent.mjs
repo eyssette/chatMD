b5cc3beb010f5b5a3501373539e185248650140e
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-04-30 04:16:54 +0200

    refactor(chatbot) extractMainContent modularisé

--- app/js/core/chatbot/parsers/extractMainContent.mjs
+++ app/js/core/chatbot/parsers/extractMainContent.mjs
@@ -1,85 +1,66 @@
 import { detectChoiceOption } from "./detectChoiceOption.mjs";
+import { detectedResponseTitle } from "./detectResponseTitle.mjs";
 import {
-	detectedResponseTitle,
-	isStructureTitle,
-} from "./detectResponseTitle.mjs";
-
-const regexDynamicContentIfBlock = /`if (.*?)`/;
+	handleNewResponseTitle,
+	handleKeywords,
+	handleDynamicContent,
+	handleChoiceOptions,
+	handleRegularContent,
+} from "./helpers/processorsMainContent.mjs";
 
 export function getMainContentInformations(
 	mdWithoutYaml,
 	indexEndIntroduction,
 	yaml,
 ) {
-	let currentResponseTitle = null;
-	let currentLiItems = [];
-	let content = [];
-	let lastOrderedList = null;
-	let listParsed = false;
-	const contentAfterFirstPart = mdWithoutYaml.substring(indexEndIntroduction);
-	const contentAfterFirstPartLines = contentAfterFirstPart.split("\n");
-	let ifCondition = "";
-	let chatbotData = [];
-	for (let line of contentAfterFirstPartLines) {
-		const choiceStatus = detectChoiceOption(line);
+	const mainContent = mdWithoutYaml.substring(indexEndIntroduction);
+	const mainContentLines = mainContent.split("\n");
+	const chatbotData = [];
+
+	const currentData = {
+		responseTitle: null,
+		keywords: [],
+		content: [],
+		choiceOptions: null,
+		listParsed: false,
+		ifCondition: "",
+	};
+
+	for (let line of mainContentLines) {
+		// Gestion des titres de réponse
 		if (detectedResponseTitle(line, yaml)) {
-			// Gestion des identifiants de réponse, et début de traitement du contenu de chaque réponse
-			if (currentResponseTitle) {
-				chatbotData.push([
-					currentResponseTitle,
-					currentLiItems,
-					content,
-					lastOrderedList,
-				]);
-			}
-			currentResponseTitle = line
-				.replace(detectedResponseTitle(line, yaml), "")
-				.trim();
-			currentLiItems = [];
-			lastOrderedList = null;
-			listParsed = false;
-			content = [];
-		} else if (line.startsWith("- ") && !listParsed) {
-			// Gestion des listes
-			currentLiItems.push(line.replace("- ", "").trim());
-		} else if (yaml.dynamicContent && regexDynamicContentIfBlock.test(line)) {
-			// Cas des blocs dynamiques conditionnels
-			ifCondition = line.match(regexDynamicContentIfBlock)[1]
-				? line.match(regexDynamicContentIfBlock)[1]
-				: "";
-			content.push(line + "\n");
-			listParsed = true;
-		} else if (yaml.dynamicContent && line.includes("`endif`")) {
-			ifCondition = "";
-			content.push(line + "\n");
-			listParsed = true;
-		} else if (choiceStatus.isChoice) {
-			// Cas des listes ordonnées
-			listParsed = false;
-			if (!lastOrderedList) {
-				lastOrderedList = [];
-			}
-			const listContent = line.replace(/^\d+(\.|\))\s/, "").trim();
-			let link = listContent.replace(/^\[.*?\]\(/, "").replace(/\)$/, "");
-			link = yaml.obfuscate ? btoa(link) : link;
-			const text = listContent.replace(/\]\(.*/, "").replace(/^\[/, "");
-			lastOrderedList.push([text, link, choiceStatus.isRandom, ifCondition]);
-		} else if (line.length > 0 && !isStructureTitle(line, yaml)) {
-			// Gestion du reste du contenu de la réponse
-
-			// Pour définir le contenu d'une réponse, le chatbot ne prend pas en compte les lignes qui contiennent un titre qui sert simplent à structurer le chatbot (pour le créateur, sans affichage dans le chatbot côté utilisateur)
-
-			// Possibilité de faire des liens à l'intérieur du contenu vers une réponse
-			line = line.replaceAll(/\[(.*?)\]\((#.*?)\)/g, '<a href="$2">$1</a>');
-			content.push(line);
-			listParsed = true;
+			handleNewResponseTitle(line, yaml, currentData, chatbotData);
+			continue;
 		}
+
+		// Gestion des mots clés
+		if (line.startsWith("- ") && !currentData.listParsed) {
+			handleKeywords(line, currentData);
+			continue;
+		}
+
+		// Gestion des blocs conditionnels si on a du contenu dynamique
+		if (handleDynamicContent(line, currentData, yaml)) {
+			continue;
+		}
+
+		// Gestion des éventuelles options de choix proposées en fin de réponse
+		const choiceStatus = detectChoiceOption(line);
+		if (choiceStatus.isChoice) {
+			handleChoiceOptions(line, choiceStatus, yaml, currentData);
+			continue;
+		}
+
+		// Gestion du reste du contenu de la réponse
+		handleRegularContent(line, yaml, currentData);
 	}
+
 	chatbotData.push([
-		currentResponseTitle,
-		currentLiItems,
-		content,
-		lastOrderedList,
+		currentData.responseTitle,
+		currentData.keywords,
+		currentData.content,
+		currentData.choiceOptions,
 	]);
+
 	return chatbotData;
 }
---
+++ app/js/core/chatbot/parsers/helpers/processorsMainContent.mjs
@@ -0,0 +1,79 @@
+import {
+	detectedResponseTitle,
+	isStructureTitle,
+} from "../detectResponseTitle.mjs";
+
+const regexDynamicContentIfBlock = /`if (.*?)`/;
+
+// Gestion des titres de réponse
+export function handleNewResponseTitle(line, yaml, currentData, chatbotData) {
+	if (currentData.responseTitle) {
+		chatbotData.push([
+			currentData.responseTitle,
+			currentData.keywords,
+			currentData.content,
+			currentData.choiceOptions,
+		]);
+	}
+	currentData.responseTitle = line
+		.replace(detectedResponseTitle(line, yaml), "")
+		.trim();
+	currentData.keywords = [];
+	currentData.choiceOptions = null;
+	currentData.content = [];
+	currentData.listParsed = false;
+}
+
+// Gestion des mots clés
+export function handleKeywords(line, currentData) {
+	currentData.keywords.push(line.replace("- ", "").trim());
+}
+
+// Gestion des blocs conditionnels si on a du contenu dynamique
+export function handleDynamicContent(line, currentData, yaml) {
+	if (yaml.dynamicContent && regexDynamicContentIfBlock.test(line)) {
+		currentData.ifCondition =
+			(line.match(regexDynamicContentIfBlock) &&
+				line.match(regexDynamicContentIfBlock)[1]) ||
+			"";
+
+		currentData.content.push(line + "\n");
+		currentData.listParsed = true;
+		return true;
+	}
+	if (yaml.dynamicContent && line.includes("`endif`")) {
+		currentData.ifCondition = "";
+		currentData.content.push(line + "\n");
+		currentData.listParsed = true;
+		return true;
+	}
+	return false;
+}
+
+// Gestion des éventuelles options de choix proposées en fin de réponse
+export function handleChoiceOptions(line, choiceStatus, yaml, currentData) {
+	currentData.listParsed = false;
+	if (!currentData.choiceOptions) {
+		currentData.choiceOptions = [];
+	}
+	const listContent = line.replace(/^\d+(\.|\))\s/, "").trim();
+	let link = listContent.replace(/^\[.*?\]\(/, "").replace(/\)$/, "");
+	link = yaml.obfuscate ? btoa(link) : link;
+	const text = listContent.replace(/\]\(.*/, "").replace(/^\[/, "");
+	currentData.choiceOptions.push([
+		text,
+		link,
+		choiceStatus.isRandom,
+		currentData.ifCondition,
+	]);
+}
+
+// Gestion du reste du contenu de la réponse
+export function handleRegularContent(line, yaml, currentData) {
+	if (line.length > 0 && !isStructureTitle(line, yaml)) {
+		// Gestion des liens à l'intérieur du contenu, vers une réponse
+		line = line.replaceAll(/\[(.*?)\]\((#.*?)\)/g, '<a href="$2">$1</a>');
+		currentData.content.push(line);
+		currentData.listParsed = true;
+	}
+}
