597441ba44eda4adcaf460f0215abbb2431d04a6
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-05-12 03:14:35 +0200

    edit: changement ordre des imports

--- app/js/main.mjs
+++ app/js/main.mjs
@@ -1,5 +1,5 @@
 import "../css/styles.css";
-import { createChatbot } from "./core/chatbot/createChatbot.mjs";
 import defaultMd from "../index.md";
+import { createChatbot } from "./core/chatbot/createChatbot.mjs";
 
 createChatbot(defaultMd);
