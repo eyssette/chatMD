f52436bfc71f6773811d045fbfd194035ea984cf
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-05-15 23:04:16 +0200

    chore: configuration d'ESlint avec codeceptJS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import codeceptjsPlugin from "eslint-plugin-codeceptjs";
 
 export default [
 	{
 		languageOptions: {
-			globals: { ...globals.browser, ...globals.jasmine },
+			globals: {
+				...globals.browser,
+				...globals.jasmine,
+			},
 			ecmaVersion: 2018,
 		},
 	},
 	pluginJs.configs.recommended,
 	{
-		files: ["/app/**/*.js", "/app/**/*.mjs"],
+		files: ["/app/**/*.js", "/app/**/*.mjs", "tests/**/*.js"],
 		rules: {
+			...codeceptjsPlugin.configs.recommended.rules,
 			semi: ["error", "always"],
 			indent: "off",
 			quotes: ["error", "double", { avoidEscape: true }],
