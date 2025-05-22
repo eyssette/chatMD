e1a33b327c740352181ddbb2ba1bc993d8de4740
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-05-22 14:58:06 +0200

    fix(markdown): problème résolu en cas de message avec un prompt et le prompt qui commence en premier

--- app/js/markdown/custom/directives/useLLM/processMessageWithPrompt.mjs
+++ app/js/markdown/custom/directives/useLLM/processMessageWithPrompt.mjs
@@ -1,6 +1,7 @@
 import { getAnswerFromLLM } from "../../../../ai/getAnswerFromLLM.mjs";
 import { displayMessage } from "../../../../core/messages/displayMessage.mjs";
 import { processPromptWithRAG } from "./processPromptWithRAG.mjs";
+import { appendMessageToContainer } from "../../../../core/messages/helpers/dom.mjs";
 
 // Traite chaque partie d’un message découpé (Markdown / LLM)
 export async function processMessageWithPrompt(
@@ -9,13 +10,17 @@ export async function processMessageWithPrompt(
 	messageElement,
 	isUser,
 ) {
-	for (const section of sequence) {
+	for (let i = 0; i < sequence.length; i++) {
+		const section = sequence[i];
 		const type = section.type;
 		let content = section.content;
 		const sectionElement = document.createElement("div");
 
 		try {
 			if (type === "prompt") {
+				if (i === 0) {
+					appendMessageToContainer(sectionElement, messageElement);
+				}
 				let RAGinformations = "";
 				if (content.includes("!RAG: ")) {
 					const promptWithRag = await processPromptWithRAG(chatbot, content);
