3233057b4f1d55420e6b35f973cc10e6d6dd82fd
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-06-04 00:36:15 +0200

    fix(chatbot): ajout automatique de l'extension .md au nom de fichier si le fetch a échoué la première fois
    Si on déploie ChatMD avec des fichiers dans son dépôt, on peut alors utiliser des URLs plus significatives, sans avoir à ajouter le .md dans l'URL

--- app/js/core/chatbot/helpers/fetch.mjs
+++ app/js/core/chatbot/helpers/fetch.mjs
@@ -13,12 +13,24 @@ export async function fetchContent(url) {
 // Pour récupérer le contenu d'un fichier à partir d'une URL en utilisant un proxy si besoin
 export async function fetchContentWithProxyIfNeeded(url) {
 	try {
+		// On essaie de récupérer le contenu directement de l'URL
 		return await fetchContent(url);
-	} catch (error) {
-		const proxiedUrl = config.corsProxy + url;
-		console.warn(`Fetch direct échoué, tentative avec proxy : ${proxiedUrl}`);
-		console.log(error);
-		return await fetchContent(proxiedUrl);
+	} catch (initialFetchError) {
+		try {
+			// Si cela ne marche pas, on essaie d'ajouter l'extension .md à la fin de l'URL
+			// Si on déploie ChatMD avec des fichiers dans son dépôt, on peut alors utiliser des URLs plus significatives, sans avoir à ajouter le .md dans l'URL
+			return await fetchContent(`${url}.md`);
+		} catch (mdFetchError) {
+			// Si cela ne marche pas, on reprend l'URL initiale et on essaie avec un proxy CORS
+			const proxiedUrl = config.corsProxy + url;
+			try {
+				return await fetchContent(proxiedUrl);
+			} catch (proxyFetchError) {
+				console.error("Problème avec le fetch initial :", initialFetchError);
+				console.error("Échec de l'ajout de l'extension .md :", mdFetchError);
+				console.error("Échec de l'ajout du proxy :", proxyFetchError);
+			}
+		}
 	}
 }
 
