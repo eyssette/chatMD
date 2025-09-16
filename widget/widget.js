3f5380851ebfc9af36d136dc050baec3cd82e8c7
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-09-17 01:07:37 +0200

    fix(widget): amélioration de l'apparence du widget
    - résout un problème d'affichage sur des écrans avec une faible hauteur
    - améliore le rendu sur écran de portable

--- widget/widget.js
+++ widget/widget.js
@@ -18,11 +18,12 @@ const widgetStyle = `
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	height: 100%;
 	background-color: #f4f4f4;
 	padding: 10px;
 	border-radius:50%;
 	z-index:1;
+	width: inherit;
+	height: inherit;
 }
 
 #chatmdWidget img {
@@ -35,18 +36,28 @@ const widgetStyle = `
 	bottom:150px;
 	display: none;
 	width: 400px;
-	height: 600px;
+	height: min(600px,70vh);
 	border-radius: 15px;
 	background: linear-gradient(90deg, rgb(239, 242, 247) 0%, 7.60286%, rgb(237, 240, 249) 15.2057%, 20.7513%, rgb(235, 239, 248) 26.297%, 27.6386%, rgb(235, 239, 248) 28.9803%, 38.2826%, rgb(231, 237, 249) 47.585%, 48.1216%, rgb(230, 236, 250) 48.6583%, 53.1306%, rgb(228, 236, 249) 57.6029%, 61.5385%, rgb(227, 234, 250) 65.4741%, 68.7835%, rgb(222, 234, 250) 72.093%, 75.7603%, rgb(219, 230, 248) 79.4275%, 82.8265%, rgb(216, 229, 248) 86.2254%, 87.8354%, rgb(213, 228, 249) 89.4454%, 91.8605%, rgb(210, 226, 249) 94.2755%, 95.4383%, rgb(209, 225, 248) 96.6011%, 98.3005%, rgb(208, 224, 247) 100%);
 }
 #chatmdWidget div:nth-of-type(2) iframe {
 	padding: 10px;
+	height:inherit;
 }
 
 @media screen and (max-width: 500px) {
 	#chatmdWidget {
-		bottom: 10px;
-		right: 10px;
+		bottom: 30px;
+		right: 30px;
+		width: 60px;
+		height: 60px;
+	}
+	#chatmdWidget div:nth-of-type(1) {
+		width: 60px;
+		height: 60px;
+	}
+	#chatmdWidget img {
+		width:90%;
 	}
 	#chatmdWidget div:nth-of-type(2) {
 		right: 0px;
