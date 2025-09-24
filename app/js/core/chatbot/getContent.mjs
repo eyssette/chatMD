40de372802bc6bc8f137dc2a44ad4bb9ee0ab3ae
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-09-24 09:05:11 +0200

    fix(core): traitement de la propriété "include" dans le YAML aussi pour le fichier index.md par défaut

--- app/js/core/chatbot/getContent.mjs
+++ app/js/core/chatbot/getContent.mjs
@@ -33,18 +33,18 @@ export async function getContent(defaultMd, params) {
 			// Récupération du contenu du fichier
 			content = await fetchContentWithProxyIfNeeded(sourceChatBot);
 		}
-		yaml = processYAML(content);
-		if (yaml && yaml.include) {
-			// Cas où on doit inclure le contenu d'autres fichiers à la suite du premier (fichiers définis dans l'en-tête YAML du premier fichier, avec le paramètre "include")
-			const includes =
-				typeof yaml.include === "object"
-					? yaml.include
-					: { include: yaml.include };
-			const contentToInclude = await fetchContentFromMultipleSources(includes);
-			content = content + "\n\n" + contentToInclude;
-		}
-		// On valide le Markdown, sinon on obtient un message d'erreur
-		content = validateMarkdown(content, defaultMd);
 	}
+	yaml = processYAML(content);
+	if (yaml && yaml.include) {
+		// Cas où on doit inclure le contenu d'autres fichiers à la suite du premier (fichiers définis dans l'en-tête YAML du premier fichier, avec le paramètre "include")
+		const includes =
+			typeof yaml.include === "object"
+				? yaml.include
+				: { include: yaml.include };
+		const contentToInclude = await fetchContentFromMultipleSources(includes);
+		content = content + "\n\n" + contentToInclude;
+	}
+	// On valide le Markdown, sinon on obtient un message d'erreur
+	content = validateMarkdown(content, defaultMd);
 	return { markdown: content, yaml: yaml ? yaml : processYAML(content) };
 }
