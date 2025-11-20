98a886aac6cacca8e8845b3ac40e796d8139022f
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-11-20 20:07:15 +0100

    fix(core): si KaTeX est déjà chargé, on résout immédiatement la promesse d'attente du chargement de la librairie
    Évite un bug dans l'apparition des messages quand on utilise une réponse par un LLM  + des maths

--- app/js/core/messages/helpers/plugins/waitForKatex.mjs
+++ app/js/core/messages/helpers/plugins/waitForKatex.mjs
@@ -1,6 +1,10 @@
 // On attend que la librairie de gestion de Latex soit disponible (avec un maximum d'essais pour éviter une boucle infinie)
 export function waitForKaTeX() {
 	return new Promise((resolve) => {
+		if (window.katex) {
+			resolve();
+			return;
+		}
 		let attempts = 0;
 		const interval = setInterval(() => {
 			if (window.katex || attempts > 10) {
