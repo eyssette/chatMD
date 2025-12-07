689e751f4755a070be05c6f828927b1b313c088b
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-12-07 14:44:55 +0100

    refactor(core): refactorisation de computeResponseScore() - fonction de calcul du score pour un keyword mise à part

---
+++ app/js/core/interactions/helpers/findBestResponse/computeKeywordScore.mjs
@@ -0,0 +1,67 @@
+import {
+	hasLevenshteinDistanceLessThan,
+	longestCommonSubstringWeightedLength,
+	normalizeText,
+} from "../../../../utils/nlp.mjs";
+
+const LEVENSHTEIN_THRESHOLD = 3; // Seuil de similarité (tolérance des fautes d'orthographe et des fautes de frappe)
+const WORD_LENGTH_FACTOR = 0.1; // Prise en compte de la taille des keywords (plus les keywords sont grands, plus ils doivent avoir un poids important)
+
+export function computeKeywordScore(userInput, keyword, next, options = {}) {
+	let distanceScore = 0;
+	let matchScore = 0;
+	const MATCH_SCORE_IDENTITY =
+		options && options.identity_bonus ? options.identity_bonus : 0;
+	// On prend en compte les keywords négatifs (on ne doit pas les voir dans la question de l'utilisateur)
+	const isNegativeKeyword = keyword.startsWith("! ");
+	keyword = keyword.replace(/^!\s/, "");
+	keyword = normalizeText(keyword, { keepCase: true });
+	if (userInput.includes(keyword)) {
+		// Test de l'identité stricte
+		let strictIdentityMatch = false;
+		if (next.needsProcessing) {
+			// Si on utilise la directive !Next, on vérifie que le keyword n'est pas entouré de lettres ou de chiffres dans le message de l'utilisateur
+			const regexStrictIdentityMatch = new RegExp(`\\b${keyword}\\b`);
+			if (regexStrictIdentityMatch.test(userInput)) {
+				strictIdentityMatch = true;
+			}
+		} else {
+			strictIdentityMatch = true;
+		}
+		if (strictIdentityMatch) {
+			// En cas d'identité stricte, on monte le score d'une valeur définie par MATCH_SCORE_IDENTITY, ou alors on le diminue si on avait un keyword négatif
+			matchScore = isNegativeKeyword
+				? matchScore - MATCH_SCORE_IDENTITY * 2
+				: matchScore + MATCH_SCORE_IDENTITY;
+			// On privilégie les correspondances sur les keywords plus longs
+			matchScore = matchScore + keyword.length * WORD_LENGTH_FACTOR;
+		}
+	} else if (
+		(userInput.length > 5) &
+		(keyword.length > 4 && !isNegativeKeyword)
+	) {
+		// Sinon : test de la similarité (seulement si le message de l'utilisateur n'est pas très court)
+		// On calcule la distance de Levenshtein entre le keyword et la question de l'utilisateur (en parcourant les n-grammes du message de l'utilisateur et en prenant en compte la longueur du n-gramme ; avec n = nombre de mots du keyword)
+		const levenshteinDistance = hasLevenshteinDistanceLessThan(
+			userInput,
+			keyword,
+			LEVENSHTEIN_THRESHOLD,
+			WORD_LENGTH_FACTOR,
+		);
+		distanceScore =
+			levenshteinDistance > 1
+				? distanceScore + levenshteinDistance
+				: distanceScore;
+		if (!next.needsProcessing) {
+			// On prend en compte la plus longue chaîne commune de caractères (sauf si on doit passer au message seulement s'il y a présence du keyword [cas d'un quiz] : dans ce cas, on doit être plus strict et tester seulement la proximité avec la distance de Levenshtein pour simplement autoriser quelques fautes d'orthographe)
+			distanceScore =
+				distanceScore +
+				longestCommonSubstringWeightedLength(
+					userInput,
+					keyword,
+					WORD_LENGTH_FACTOR,
+				);
+		}
+	}
+	return { matchScore, distanceScore };
+}
--- app/js/core/interactions/helpers/findBestResponse/computeResponseScore.mjs
+++ app/js/core/interactions/helpers/findBestResponse/computeResponseScore.mjs
@@ -1,27 +1,22 @@
-import {
-	hasLevenshteinDistanceLessThan,
-	cosineSimilarity,
-	longestCommonSubstringWeightedLength,
-	normalizeText,
-} from "../../../../utils/nlp.mjs";
+import { cosineSimilarity } from "../../../../utils/nlp.mjs";
+import { computeKeywordScore } from "./computeKeywordScore.mjs";
 
-const LEVENSHTEIN_THRESHOLD = 3; // Seuil de similarité (tolérance des fautes d'orthographe et des fautes de frappe)
 const MATCH_SCORE_IDENTITY = 30; // Pour régler le fait de privilégier l'identité d'un keyword à la simple similarité
-const WORD_LENGTH_FACTOR = 0.1; // Prise en compte de la taille des keywords (plus les keywords sont grands, plus ils doivent avoir un poids important)
 
 function buildKeywordsList(next, response) {
-	// Si on a la directive !Next, alors on ne teste pas la correspondance avec le titre, mais seulement avec les keywords (sauf s'il n'y a pas de keyword)
+	// Si on a la directive !Next, on inclut seulement les keywords dans la liste des termes à tester (sauf s'il n'y a pas de keyword)
 	// Sinon on inclut le titre
-	// On met tout en minuscule
-	const keywords =
+
+	const useOnlyKeywords =
 		next.needsProcessing &&
 		response.keywords.length > 0 &&
-		next.ignoreKeywords !== true
-			? response.keywords.map((keyword) => keyword.toLowerCase())
-			: response.keywords
-					.concat(response.title)
-					.map((keyword) => keyword.toLowerCase());
-	return keywords;
+		next.ignoreKeywords !== true;
+
+	const baseList = useOnlyKeywords
+		? response.keywords
+		: [...response.keywords, response.title];
+
+	return baseList.map((k) => k.toLowerCase());
 }
 
 function calculateCosineSimilarityScore(
@@ -37,83 +32,14 @@ function calculateCosineSimilarityScore(
 	return cosSim ? cosSim + 0.5 : 0;
 }
 
-export function computeResponseScore({
-	chatbot,
-	userInput,
+function adjustScore(
 	response,
-	responseIndex,
+	matchScore,
+	distanceScore,
+	bestDistanceScore,
+	next,
 	yaml,
-}) {
-	const next = chatbot.nextMessage;
-	let bestDistanceScore = 0;
-	// Si on a la directive !Next, alors on ne teste pas la correspondance avec le titre, mais seulement avec les keywords (sauf s'il n'y a pas de keyword)
-	// Sinon on inclut le titre
-	// On met tout en minuscule
-	const keywords = buildKeywordsList(next, response);
-	let matchScore = 0;
-	let distanceScore = 0;
-	// Si le YAML indique de faire une recherche dans le contenu avec la similarité vectorielle, on prend comme base de score le cosine similarity entre le message de l'utilisateur et le contenu vectoriel de la réponse
-	if (yaml && yaml.searchInContent) {
-		matchScore = calculateCosineSimilarityScore(
-			chatbot,
-			userInput,
-			responseIndex,
-			next,
-		);
-	}
-	for (let keyword of keywords) {
-		// On prend en compte les keywords négatifs (on ne doit pas les voir dans la question de l'utilisateur)
-		const isNegativeKeyword = keyword.startsWith("! ");
-		keyword = keyword.replace(/^!\s/, "");
-		keyword = normalizeText(keyword, { keepCase: true });
-		if (userInput.includes(keyword)) {
-			// Test de l'identité stricte
-			let strictIdentityMatch = false;
-			if (next.needsProcessing) {
-				// Si on utilise la directive !Next, on vérifie que le keyword n'est pas entouré de lettres ou de chiffres dans le message de l'utilisateur
-				const regexStrictIdentityMatch = new RegExp(`\\b${keyword}\\b`);
-				if (regexStrictIdentityMatch.test(userInput)) {
-					strictIdentityMatch = true;
-				}
-			} else {
-				strictIdentityMatch = true;
-			}
-			if (strictIdentityMatch) {
-				// En cas d'identité stricte, on monte le score d'une valeur définie par MATCH_SCORE_IDENTITY, ou alors on le diminue si on avait un keyword négatif
-				matchScore = isNegativeKeyword
-					? matchScore - MATCH_SCORE_IDENTITY * 2
-					: matchScore + MATCH_SCORE_IDENTITY;
-				// On privilégie les correspondances sur les keywords plus longs
-				matchScore = matchScore + keyword.length * WORD_LENGTH_FACTOR;
-			}
-		} else if (
-			(userInput.length > 5) &
-			(keyword.length > 4 && !isNegativeKeyword)
-		) {
-			// Sinon : test de la similarité (seulement si le message de l'utilisateur n'est pas très court)
-			// On calcule la distance de Levenshtein entre le keyword et la question de l'utilisateur (en parcourant les n-grammes du message de l'utilisateur et en prenant en compte la longueur du n-gramme ; avec n = nombre de mots du keyword)
-			const levenshteinDistance = hasLevenshteinDistanceLessThan(
-				userInput,
-				keyword,
-				LEVENSHTEIN_THRESHOLD,
-				WORD_LENGTH_FACTOR,
-			);
-			distanceScore =
-				levenshteinDistance > 1
-					? distanceScore + levenshteinDistance
-					: distanceScore;
-			if (!next.needsProcessing) {
-				// On prend en compte la plus longue chaîne commune de caractères (sauf si on doit passer au message seulement s'il y a présence du keyword [cas d'un quiz] : dans ce cas, on doit être plus strict et tester seulement la proximité avec la distance de Levenshtein pour simplement autoriser quelques fautes d'orthographe)
-				distanceScore =
-					distanceScore +
-					longestCommonSubstringWeightedLength(
-						userInput,
-						keyword,
-						WORD_LENGTH_FACTOR,
-					);
-			}
-		}
-	}
+) {
 	// si on a un score de distance négatif, c'est qu'il y avait des keywords négatifs : donc le matchscore doit être égal à 0
 	if (distanceScore < 0) {
 		matchScore = 0;
@@ -132,6 +58,54 @@ export function computeResponseScore({
 	if (matchScore > 0.5 && next.needsProcessing && response.title == next.goto) {
 		matchScore = matchScore + MATCH_SCORE_IDENTITY;
 	}
+	return { matchScore, bestDistanceScore };
+}
+
+export function computeResponseScore({
+	chatbot,
+	userInput,
+	response,
+	responseIndex,
+	yaml,
+}) {
+	const next = chatbot.nextMessage;
+	const keywords = buildKeywordsList(next, response);
+
+	let bestDistanceScore = 0;
+	let matchScore = 0;
+	let distanceScore = 0;
+
+	// Si le YAML indique de faire une recherche dans le contenu avec la similarité vectorielle, on prend comme base de score le cosine similarity entre le message de l'utilisateur et le contenu vectoriel de la réponse
+	if (yaml && yaml.searchInContent) {
+		matchScore = calculateCosineSimilarityScore(
+			chatbot,
+			userInput,
+			responseIndex,
+			next,
+		);
+	}
+
+	// On calcule les scores pour chaque keyword
+	for (let keyword of keywords) {
+		const keywordScore = computeKeywordScore(userInput, keyword, next, {
+			identity_bonus: MATCH_SCORE_IDENTITY,
+		});
+		matchScore = matchScore + keywordScore.matchScore;
+		distanceScore = distanceScore + keywordScore.distanceScore;
+	}
+
+	// On ajuste le score
+	const adjustedScore = adjustScore(
+		response,
+		matchScore,
+		distanceScore,
+		bestDistanceScore,
+		next,
+		yaml,
+	);
+
+	matchScore = adjustedScore.matchScore;
+	bestDistanceScore = adjustedScore.bestDistanceScore;
 
 	return matchScore;
 }
